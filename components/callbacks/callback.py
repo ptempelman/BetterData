7591912d5a4bdc68a587a6f2b5b4bca09ac61ce5
Philip Tempelman <tempelmanphilip@gmail.com>
2023-10-07 17:55:42 -0400

    minor clean up of callbacks

--- components/callbacks/callback.py
+++ components/callbacks/callback.py
@@ -53,62 +53,40 @@ def get_callbacks(app):
         return col
 
     @callback(
-        [Output(component_id='graph-container-1', component_property='children'),
-        Output(component_id='graph-container-2', component_property='children'),
-        Output(component_id='graph-container-3', component_property='children'),
-        Output(component_id='graph-container-4', component_property='children'),
-        Output('open-button-1', 'style'),
-        Output('open-button-2', 'style'),
-        Output('open-button-3', 'style'),
-        Output('open-button-4', 'style'),
-        Output(component_id='graph-container-1', component_property='style'),
-        Output(component_id='graph-container-2', component_property='style'),
-        Output(component_id='graph-container-3', component_property='style'),
-        Output(component_id='graph-container-4', component_property='style'),
-        Output("modal", "is_open", allow_duplicate=True)],
-        [Input(component_id='add-graph-button', component_property='n_clicks')],
+        [Output(f'graph-container-{i + 1}', 'children') for i in range(4)] + \
+        [Output(f'open-button-{i + 1}', 'style') for i in range(4)] + \
+        [Output(f'graph-container-{i + 1}', 'style') for i in range(4)] + \
+        [Output("modal", "is_open", allow_duplicate=True)],
+        [Input('add-graph-button', 'n_clicks')],
         [State('hidden-div', 'children'),
         State('hidden-div-xdropdown', 'children'),
-        State('hidden-div-ydropdown', 'children'),
-        State('open-button-1', 'style'),
-        State('open-button-2', 'style'),
-        State('open-button-3', 'style'),
-        State('open-button-4', 'style'),
-        State('graph-container-1', 'children'),
-        State('graph-container-2', 'children'),
-        State('graph-container-3', 'children'),
-        State('graph-container-4', 'children'),
-        State('graph-container-1', 'style'),
-        State('graph-container-2', 'style'),
-        State('graph-container-3', 'style'),
-        State('graph-container-4', 'style'),
-        State('hidden-div-dataset', 'children')
-        ],
+        State('hidden-div-ydropdown', 'children')] + \
+        [State(f'open-button-{i + 1}', 'style') for i in range(4)] + \
+        [State(f'graph-container-{i + 1}', 'children') for i in range(4)] + \
+        [State(f'graph-container-{i + 1}', 'style') for i in range(4)] + \
+        [State('hidden-div-dataset', 'children')],
         prevent_initial_call=True
         )
     def add_graph(n, btn_id, xcol, ycol, vis1, vis2, vis3, vis4, gc1, gc2, gc3, gc4, gv1, gv2, gv3, gv4, ds):
 
-        graph = dcc.Graph(figure=px.histogram(pd.read_csv(osp.join(osp.dirname(data.__file__), ds)), x=xcol, y=ycol, histfunc='avg'), className='main-graph', \
+        graph = dcc.Graph(figure=px.histogram(pd.read_csv(osp.join(osp.dirname(data.__file__), ds)), x=xcol, y=ycol, histfunc='avg', template='plotly_dark'), className='main-graph', \
                             config = {'displaylogo': False, 'modeBarButtonsToRemove': ['zoom', 'pan', 'select2d', 'lasso2d', 'autoscale']})
         
         inv = {'display': 'none'}
         vis = {'display': 'unset'}
-        if btn_id == 'open-button-1':
-            return [graph], gc2, gc3, gc4, \
-                inv, vis2, vis3, vis4, \
-                vis, gv2, gv3, gv4, False
-        if btn_id == 'open-button-2':
-            return gc1, [graph], gc3, gc4, \
-                vis1, inv, vis3, vis4, \
-                gv1, vis, gv3, gv4, False
-        if btn_id == 'open-button-3':
-            return gc1, gc2, [graph], gc4, \
-                vis1, vis2, inv, vis4, \
-                gv1, gv2, vis, gv4, False
-        else:
-            return gc1, gc2, gc3, [graph], \
-                vis1, vis2, vis3, inv, \
-                gv1, gv2, gv3, vis, False
+
+        idx = int(btn_id[-1]) - 1
+
+        graphs = [gc1, gc2, gc3, gc4]
+        button_vis = [vis1, vis2, vis3, vis4]
+        graph_vis = [gv1, gv2, gv3, gv4]
+
+        graphs[idx] = [graph]
+        button_vis[idx] = inv
+        graph_vis[idx] = vis
+
+        return graphs + button_vis + graph_vis + [False]
+
 
     @app.callback(
         [Output("modal", "is_open", allow_duplicate=True),
@@ -117,7 +95,7 @@ def get_callbacks(app):
             Input("open-button-1", "n_clicks"),
             Input("open-button-2", "n_clicks"),
             Input("open-button-3", "n_clicks"),
-            Input("open-button-4", "n_clicks"),
+            Input("open-button-4", "n_clicks")
         ],
         [State("modal", "is_open")],
         prevent_initial_call=True,
