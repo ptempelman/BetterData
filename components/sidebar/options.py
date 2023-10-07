00df23f6d3042050a5e836ac13bb645975f847a4
Philip Tempelman <tempelmanphilip@gmail.com>
2023-10-07 14:10:37 -0400

    fix sidebar styling bug and make graph options dependent on selected dataset

--- components/sidebar/options.py
+++ components/sidebar/options.py
@@ -1,11 +1,11 @@
-from dash import html
+from dash import html, dcc
 import os
 
 import data
 
 def render_sidebar_dataset_options():
     return [
-        html.Div(className='sidebar-option', children=[
+        html.Button(id=f'dynamic-sidebar-option-{filename[:-4]}', n_clicks=0, className='sidebar-option', children=[
             html.Div(filename, className='sidebar-text')
         ])
         for filename in os.listdir(os.path.dirname(data.__file__)) if filename.endswith('.csv')
