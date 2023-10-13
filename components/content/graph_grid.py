e1e77467c263d76b9c32fc63580df9c65871e280
Philip Tempelman <tempelmanphilip@gmail.com>
2023-10-13 00:22:54 -0400

    update graph_grid layout to accomodate draggable resizable components

--- components/content/graph_grid.py
+++ components/content/graph_grid.py
@@ -1,5 +1,8 @@
 from dash import html
 import dash_bootstrap_components as dbc
+import dash_draggable
+import pandas as pd
+from dash import dcc
 
 
 def render_graph_grid():
@@ -22,8 +25,8 @@ def render_graph_grid():
                 id="graph-view-container",
                 className="grid-container",
                 children=[
-                    html.Div(
-                        className="grid-item",
+                    dash_draggable.ResponsiveGridLayout(
+                        id="draggable",
                         children=[
                             html.Div(
                                 [
@@ -69,10 +72,66 @@ def render_graph_grid():
                                 id=f"graph-square-{i}",
                                 className="graph-square",
                             )
+                            for i in range(1, 5)
                         ],
                     )
-                    for i in range(1, 5)
                 ],
             ),
+            # html.Div(
+            #     id="graph-view-container",
+            #     className="grid-container",
+            #     children=[
+            #         html.Div(
+            #             className="grid-item",
+            #             children=[
+            #                 html.Div(
+            #                     [
+            #                         dbc.Button(
+            #                             [html.Div("+", className="plus-graph")],
+            #                             className="add-graph-area",
+            #                             id=f"open-button-{i}",
+            #                             n_clicks=0,
+            #                         ),
+            #                         html.Div(
+            #                             id=f"filled-container-{i}",
+            #                             className="filled-container",
+            #                             style={"display": "none"},
+            #                             children=[
+            #                                 html.Div(
+            #                                     id=f"graph-menu-options-{i}",
+            #                                     className="graph-menu-options",
+            #                                     children=[
+            #                                         html.Button(
+            #                                             id=f"graph-menu-edit-{i}",
+            #                                             className="graph-menu-button",
+            #                                             children=html.Img(
+            #                                                 className="graph-menu-button-img",
+            #                                                 src="assets/edit_FILL0_wght400_GRAD0_opsz24.svg",
+            #                                             ),
+            #                                         ),
+            #                                         html.Button(
+            #                                             id=f"graph-menu-delete-{i}",
+            #                                             className="graph-menu-button",
+            #                                             children=html.Img(
+            #                                                 src="assets/delete_FILL0_wght400_GRAD0_opsz24.svg"
+            #                                             ),
+            #                                         ),
+            #                                     ],
+            #                                 ),
+            #                                 html.Div(
+            #                                     id=f"graph-container-{i}",
+            #                                     className="graph-container",
+            #                                 ),
+            #                             ],
+            #                         ),
+            #                     ],
+            #                     id=f"graph-square-{i}",
+            #                     className="graph-square",
+            #                 )
+            #             ],
+            #         )
+            #         for i in range(1, 5)
+            #     ],
+            # ),
         ],
     )
